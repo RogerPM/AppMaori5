4510008cbcfd31dcdb40ab86079f34233ef0a3d2
JDGetsuga <jdavid_wolf@hotmail.com>
2018-01-16 17:27:58 -0500

    usuario

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -26,18 +26,19 @@ ActiveAdmin.register User do
 
   form do |f|
     f.inputs do
-      f.input :name
-      f.input :last_name
-      f.input :identification_card
-      f.input :address
-      f.input :phone
-      f.input :contract_date
-      f.input :contract_date_end
-      f.input :position
-      f.input :state
-      f.input :email
-      f.input :password
-      f.input :password_confirmation
+      f.input :name, :label => "Nombre"
+      f.input :last_name, :label => "Apellidos"
+      f.input :identification_card, :label => "Cedula"
+      f.input :address, :label => "Direccion"
+      f.input :phone, :label => "Telefono"
+      f.input :contract_date, :label => "Fecha de Contratacion"
+      f.input :contract_date_end, :label => "Fecha de renuncia"
+      f.input :position, :label => "Cargo", as: :selectize, :collection => [ ["Administrador","admin"],["Cajero","saler"],["Entrenador","coach"]]
+      
+      f.input :email, :label => "Email"
+      f.input :password, :label => "Contraseña"
+      f.input :password_confirmation, :label => "Confirmar contraseña"
+      f.input :state, :label => "Cuenta", as: :selectize, :collection => [ ["Activa",1],["Inactiva",0]]
     end
     f.actions
   end
